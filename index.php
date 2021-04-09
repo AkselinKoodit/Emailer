204f3682eea11c29abcf9fd49d9ccb5daf90ffe3
Akseli <s2100140@edu.bc.fi>
2021-04-09 12:58:05 +0300

    changed php name to index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Email sender</title>
     <link rel="stylesheet" href="webform.css" media="all">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>    
     <script src="main.js"></script>
